2acd5932a20b0a94c3e3e5e9955b1f0a06bda73d
lobo922 <lobo92@gmail.com>
2017-03-11 21:35:59 +0200

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -1,28 +1,30 @@
-/**
- * Copyright 2016-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 var bodyParser = require('body-parser');
+var xhub = require('express-x-hub');
 var express = require('express');
 var app = express();
-var xhub = require('express-x-hub');
 
-app.set('port', (process.env.PORT || 5000));
-app.listen(app.get('port'));
+var publishLike = function(page_id, photo_url) {
+  var Likes = Parse.Object.extend('Likes');
+  var like = new Likes();
+  like.set('page_id', page_id);
+  like.set('photo', photo_url);
+  like.save(null, {
+    success: ,
+    error: function(gameScore, error) {
+      console.error('Failed to publish a like, with error code: ' + error.message);
+    }
+  });
+}
 
-app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
+app.listen(5000);
+app.use(xhub({ algorithm: 'sha1', secret: '392daeb911d3b73b8ca42b8bbcf0915e' }));
 app.use(bodyParser.json());
-
 app.get('/', function(req, res) {
-  console.log(req);
-  res.send('It works!');
+  console.info(req);
+  res.send('It works');
 });
 
-app.get(['/facebook', '/instagram'], function(req, res) {
+app.get(['/facebook'], function(req, res) {
   if (
     req.param('hub.mode') == 'subscribe' &&
     req.param('hub.verify_token') == 'token'
@@ -34,32 +36,40 @@ app.get(['/facebook', '/instagram'], function(req, res) {
 });
 
 app.post('/facebook', function(req, res) {
-  console.log('Facebook request body:');
-
-  if (req.isXHub) {
-    console.log('request header X-Hub-Signature found, validating');
-    if (req.isXHubValid()) {
-      console.log('request header X-Hub-Signature validated');
-      res.send('Verified!\n');
-    }
-  }
-  else {
+  if(req.isXHub) {
+    if(req.isXHubValid()) {
+      if(req.body.object == 'page') {
+        req.body.entry.forEach(function(userEntry) {
+          userEntry.changes.forEach(function(change) {
+            var value = change.value;
+            if(change.field == 'feed' && value.item == 'like' && value.verb == 'add') {
+              Parse.Cloud.httpRequest({
+                url: 'https://graph.facebook.com/v2.8/' + value.user_id + '/picture?type=large&redirect=0',
+                success: function(httpResponse) {
+                  publishLike(userEntry.id, httpResponse.data.data.url);
+                },
+                error:function(httpResponse){
+                  console.error('photo retrieval failed with error: ' + httpResponse.message);
+                }
+              });
+            } else {
+              console.info('ignoring ' + value.item + ' notification from page ' + userEntry.id);
+            } // end of values check
+          });
+        });
+      } else {
+        console.error('notification does not belong to a page: ' + JSON.stringify(req.body));
+      } // end of page check
+    } else {
+      console.error('XHub could not be validated: ' + JSON.stringify(req.body));
+      res.sendStatus(403);
+    } // end of isXHubValid
+  } else {
     console.log('Warning - request header X-Hub-Signature not present or invalid');
-    res.send('Failed to verify!\n');
-    // recommend sending 401 status in production for non-validated signatures
-    // res.sendStatus(401);
-  }
-  console.log(req.body);
+    res.sendStatus(401);
+  } // end of is isXhub
 
-  // Process the Facebook updates here
   res.sendStatus(200);
 });
 
-app.post('/instagram', function(req, res) {
-  console.log('Instagram request body:');
-  console.log(req.body);
-  // Process the Instagram updates here
-  res.sendStatus(200);
-});
-
-app.listen();
+module.exports = app;
